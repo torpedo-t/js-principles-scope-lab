badcb93763d32b2ad5f4dde2450a7b4dc9f3c808
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-07-28 14:22:31 -0500

    Done.

--- index.js
+++ index.js
@@ -2,15 +2,21 @@
 // declare global variable customerName = "bob"
 var customerName = "bob"
 
-// write a function upperCaseCustomerName that accesses global variable customerName and uppercases it. should return "BOB"
+// write a function upperCaseCustomerName that accesses global variable customerName and uppercases it. modifies customerName variable to "BOB"
 function upperCaseCustomerName() {
-    let upperCaseName = customerName.toUpperCase()
-    return upperCaseName
+    customerName = customerName.toUpperCase()
 }
 // write a function setBestCustomer that when called, declares a global variable and assigns it to be 'not bob'
-
+function setBestCustomer() {
+    window.bestCustomer = 'not bob'
+}
 // see the consequences of declaring a variable in global scope, by writing a new function overwriteBestCustomer that changes that bestCustomer variable
-
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
 // now declare a constant in global scope called leastFavoriteCustomer, be sure to assign it some initial value.
-
+const leastFavoriteCustomer = 'tom'
 // now write a function called changeLeastFavoriteCustomer that attempts to change that constant - notice what Js does when you try to change the constant.
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'tim'
+}
